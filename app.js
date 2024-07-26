2f1f18166fb245612f557f92045928e18558338f
Pablo_Eduardo <141835581+pablo041@users.noreply.github.com>
2024-07-26 12:18:38 -0300

    c

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-alert('seja bem vindo ao jogo da morte );
+alert('Boas vindas ao jogo do terror');
 let numeroSecreto = 66;
 console.log('numeroSecreto')
 let chute = prompt('Escolha um número entre 1 e 100');
 
 // se chute for igual ao número secreto
 if (chute == numeroSecreto) {
-alert('Isso ai! Você descobriu o número secreto (50)');
+alert('Isso ai! Você descobriu o número secreto (66)');
 } else {
 alert('Você errou :(')
 }
