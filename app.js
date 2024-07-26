ed81d74646a6397c7c1f56642c9c90b5fc34cf7f
Pablo_Eduardo <141835581+pablo041@users.noreply.github.com>
2024-07-26 11:59:40 -0300

    ç

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 alert('Boas vindas ao jogo do número secreto');
-let numeroSecreto = 5;
+let numeroSecreto = 50;
 console.log('numeroSecreto')
-let chute = prompt('Escolha um número entre 1 e 10');
+let chute = prompt('Escolha um número entre 1 e 50');
 
 // se chute for igual ao número secreto
 if (chute == numeroSecreto) {
