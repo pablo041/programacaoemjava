5c84b219184556040dd0878aff251b088513a085
Pablo_Eduardo <141835581+pablo041@users.noreply.github.com>
2024-07-26 12:06:20 -0300

    5

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-alert('Boas vindas ao jogo do numero secreto);
-let numeroSecreto = 50;
+alert('seja bem vindo ao jogo da morte );
+let numeroSecreto = 66;
 console.log('numeroSecreto')
-let chute = prompt('Escolha um número entre 1 e 50');
+let chute = prompt('Escolha um número entre 1 e 100');
 
 // se chute for igual ao número secreto
 if (chute == numeroSecreto) {
-alert('Isso ai! Você descobriu o número secreto (5)');
+alert('Isso ai! Você descobriu o número secreto (50)');
 } else {
 alert('Você errou :(')
 }
