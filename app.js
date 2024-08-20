925b9eb82c79e046f8713489163c94066d09698e
Pablo_Eduardo <141835581+pablo041@users.noreply.github.com>
2024-08-20 07:57:55 -0300

    a

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 alert('Boas vindas ao jogo do terror');
 let numeroSecreto = 66;
 console.log('numeroSecreto')
-let chute = prompt('Escolha um número entre 1 e 100');
+let chute = prompt('Escolha um número entre 1 e 1000');
 
 // se chute for igual ao número secreto
 if (chute == numeroSecreto) {
-alert('Isso ai! Você descobriu o número secreto (66)');
+alert('Isso ai! Você descobriu o número secreto (666)');
 } else {
 alert('Você errou :(')
 }
